0a7db2baa1f855005b031de94be18048d13c2813
Januzovic <sale.business95@gmail.com>
2025-02-20 13:05:17 +0100

    Map method on pizzaData

--- src/index.js
+++ src/index.js
@@ -68,7 +68,13 @@ function Menu() {
   return (
     <main className='menu'>
       <h2 className=''>Our Menu</h2>
-      <Pizza
+
+      <ul className='pizzas'>
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
         name='Pizza Prosciutto'
         ingredients='Tomato, mozarella, ham, aragula, and burrata cheese'
         photoName='pizzas/prosciutto.jpg'
@@ -79,7 +85,7 @@ function Menu() {
         ingredients='Tomato, mozarella, and pepperoni'
         photoName='pizzas/salamino.jpg'
         price={15}
-      />
+      /> */}
     </main>
   );
 }
@@ -87,14 +93,14 @@ function Menu() {
 function Pizza(props) {
   console.log(props);
   return (
-    <div className='pizza'>
-      <img src={props.photoName} alt={props.name} />
+    <li className='pizza'>
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 function Footer() {
