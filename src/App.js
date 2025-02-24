2c5a494ea79ba1ae74c86141e2bcd59d69797c49
Januzovic <sale.business95@gmail.com>
2025-02-24 14:17:53 +0100

    Static content added

---
+++ src/App.js
@@ -0,0 +1,32 @@
+export default function App() {
+  return (
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className='list'>list</div>;
+}
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have x items on your list, and you already pack x(x%)</em>
+    </footer>
+  );
+}
