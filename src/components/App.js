3b6e6a025123ede826291cd265675b570007ec4f
Januzovic <sale.business95@gmail.com>
2025-03-05 15:46:11 +0100

    Project starter

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,20 @@
-import { useState } from 'react';
-import Logo from './Logo';
-import Form from './Form';
-import PackingList from './PackingList';
-import Stats from './Stats';
-// const initialItems = [
-//   { id: 1, description: 'Passports', quantity: 2, packed: true },
-//   { id: 2, description: 'Socks', quantity: 12, packed: false },
-// ];
-
-export default function App() {
-  const [items, setItems] = useState([]);
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]); // Adding item to items array
-  }
-
-  function handleDeleteItem(id) {
-    setItems((items) => items.filter((item) => item.id !== id)); // Removing item from items array
-  }
-
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-
-  function handleClearItems() {
-    const confirmed = window.confirm(
-      'Are you sure you want to delete all items?'
-    );
-    if (confirmed) {
-      setItems([]);
-    }
-  }
-
-  return (
-    <div className='app'>
-      <Logo />
-      <Form onAddItems={handleAddItems} />
-      <PackingList
-        items={items}
-        onDeleteItem={handleDeleteItem}
-        onToggleItem={handleToggleItem}
-        onClearItems={handleClearItems}
-      />
-      <Stats items={items} />
-    </div>
-  );
-}
+const initialFriends = [
+  {
+    id: 118836,
+    name: 'Clark',
+    image: 'https://i.pravatar.cc/48?u=118836',
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: 'Sarah',
+    image: 'https://i.pravatar.cc/48?u=933372',
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: 'Anthony',
+    image: 'https://i.pravatar.cc/48?u=499476',
+    balance: 0,
+  },
+];
