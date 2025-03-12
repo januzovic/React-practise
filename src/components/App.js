28f8d6b6b05ba4accef27b5891a20c3b4bc74343
Januzovic <sale.business95@gmail.com>
2025-03-12 10:54:23 +0100

    Style added

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,37 @@
 import { useState } from 'react';
 
 export default function App() {
-  return <div className='app'></div>;
+  return (
+    <div className=''>
+      <ToDoInput />
+      <ToDoList />
+    </div>
+  );
+}
+
+function ToDoInput() {
+  return (
+    <div className='header'>
+      <div>
+        <h2>TO DO LIST</h2>
+      </div>
+      <form className='form'>
+        <label>Things To Do</label>
+        <input type='text' />
+        <button className='btn'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+function ToDoList() {
+  return (
+    <div>
+      <div className='item'>
+        <input type='checkbox' />
+        <p>Test</p>
+        <button className='btn-close'>❌</button>
+      </div>
+    </div>
+  );
 }
